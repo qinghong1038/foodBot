c045c0f61674f1a9fb239aca9903d38a63db6db0
Raja Thiedmann <rajathie99@gmail.com>
2018-09-29 15:32:57 +0200

    #4 implemented

--- src/test/java/MyAmazingBot.java
+++ src/test/java/MyAmazingBot.java
@@ -1,32 +1,63 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-// https://github.com/rubenlagus/TelegramBots/wiki/Getting-Started
 public class MyAmazingBot extends TelegramLongPollingBot {
-	@Override
+	private Map<String, AtomicInteger> choices = new HashMap<>();
+	public static final String choice1 = "/1";
+	public static final String choice2 = "/2";
+	public static final String choice3 = "/3";
+
+	public MyAmazingBot() {
+		choices.put(choice1, new AtomicInteger(0));
+		choices.put(choice2, new AtomicInteger(0));
+		choices.put(choice3, new AtomicInteger(0));
+	}
+
 	public void onUpdateReceived(Update update) {
 		// We check if the update has a message and the message has text
 		if (update.hasMessage() && update.getMessage().hasText()) {
-			String completeResponseText = update.getMessage().getText();
-			SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-					.setChatId(update.getMessage().getChatId()).setText(completeResponseText);
+			String userMessage = update.getMessage().getText();
+
+			String responseMessageText;
+			if (!choices.keySet().contains(userMessage)) {
+				String meal1 = choice1 + " " + "Spaghetti" + "\n";
+				String meal2 = choice2 + " " + "Burrito" + "\n";
+				String decline = choice3 + " " + "Nein, danke!";
+
+				responseMessageText = meal1 + meal2 + decline;
+			} else {
+				choices.get(userMessage).incrementAndGet();
+				
+				responseMessageText = "Thanks for choosing: " + userMessage;
+			}
+
+			SendMessage myMessage = new SendMessage() // Create a SendMessage object with mandatory fields
+					.setChatId(update.getMessage().getChatId()).setText(responseMessageText);
+			
 			try {
-				execute(message); // Call method to send the message
-				System.out.println(completeResponseText);
+				execute(myMessage); // Call method to send the message
+				// System.out.println(userMessage);
 			} catch (TelegramApiException e) {
 				e.printStackTrace();
 			}
+
+			for (String choice : choices.keySet()) {
+				System.out.println(choice + " " + choices.get(choice).get());
+			}
+			System.out.println();
 		}
 	}
 
-	@Override
 	public String getBotUsername() {
 		return "RajaThiedmannsMyAmazingbot";
 	}
 
-	@Override
 	public String getBotToken() {
 		return "657674321:AAGQDtt0w6Equnwaw55R-I--dkXDHInHfeM";
 	}
